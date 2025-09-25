8f42ef30bb7997060350554d59e12fdd79a8794b
Madeleine <madejaireth2006@gmail.com>
2025-09-24 21:37:24 -0500

    prueba 2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideAnimations(), // 👈 Esto habilita las animaciones
+  ],
+})
+.catch((err) => console.error(err));
+
